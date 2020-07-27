5e88ecca6e7cf9a383d218d9eb1cdf0b3bd5c3c2
Romario Gustave <romagustave10@gmail.com>
2020-07-27 22:19:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes' do
-    @recipe = Recipe.all
-    erb:index
+      @recipes = Recipe.all
+      erb :index
+    end
+  
+    get '/recipes/new' do
+      erb :new
+    end
+  
+    post '/recipes' do
+      Recipe.new(
+        name: params[:name],
+        ingredients: params[:ingredients],
+        cook_time: params[:cook_time]
+      ).tap do |recipe|
+        recipe.save
+        redirect "/recipes/#{recipe.id}"
+      end
+    end
+  
+    get '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      erb :show
+    end
+  
+    get '/recipes/:id/edit' do
+      @recipe = Recipe.find(params[:id])
+      erb :edit
+    end
+  
+    patch '/recipes/:id' do
+      Recipe.find(params[:id]).tap do |recipe|
+        recipe.update(
+          name: params[:name],
+          ingredients: params[:ingredients],
+          cook_time: params[:cook_time]
+        )
+  
+        redirect "/recipes/#{recipe.id}"
+  
+      end
+    end
+  
+    delete '/recipes/:id/delete' do
+      Recipe.find(params[:id]).destroy
+      redirect '/recipes'
   end
 
 
